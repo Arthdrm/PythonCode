67665d23fa48b4c5a4e49a86ceeb7f635c302125
Arthdrm <artadarma37@gmail.com>
2024-09-22 17:40:02 +0800

    new script

--- test_web/scrapping_test.py
+++ test_web/scrapping_test.py
@@ -1,35 +1,30 @@
 from playwright.sync_api import sync_playwright
 
-def scrape_article_links(url, max_pages=5):
+def scrape_article_links(url):
     with sync_playwright() as p:
         # Launch a browser instance (can be 'firefox', 'webkit', or 'chromium')
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(url)
 
-        # Initialize list to store article links
         article_links = []
+        summaries_list = []
 
-       # Loop through pagination
-        for _ in range(max_pages):
-            # Collect all article links on the current page
-            links = page.locator('div.item div.img-thumb-4 a')  # Updated selector for article links
-            article_links.extend([link.get_attribute('href') for link in links.element_handles()])
-
-            # Check if there is a next page button and navigate to the next page
-            next_button = page.locator('a.next')  # Update with correct selector for the next button
-            if next_button.is_visible():
-                next_button.click()
-                page.wait_for_load_state('load')
-            else:
-                break  # No more pages to navigate
+        # Collect all article links on the current page
+        links = page.locator('article.text-card h2.title a')  # Updated selector for article links
+        summaries = page.locator('article.text-card p')
+        article_links.extend([link.get_attribute('href') for link in links.element_handles()])
+        summaries_list.extend([summary.text_content() for summary in summaries.element_handles()])
 
         # Close browser
         browser.close()
 
-    return article_links
+    return article_links, summaries_list
 
 # Example usage
-url = 'https://www.suara.com/indeks/terkini/kotaksuara/2023?page=1'  
-article_links = scrape_article_links(url)
-print(article_links)
+url = 'https://www.tempo.co/indeks/2024-09-01/'  
+(article_links, summaries) = scrape_article_links(url)
+for link in article_links:
+    print("- {}".format(link))
+for summary in summaries:
+    print("- {}".format(summary))
